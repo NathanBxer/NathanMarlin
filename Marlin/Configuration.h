1196a5790a47666a08b67f354b75d8ca0f26c93b
NathanBxer <84220506+NathanBxer@users.noreply.github.com>
2024-06-20 18:35:15 +1000

    NOZZLE_TO_PROBE_OFFSET
    changed to -45

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1598,7 +1598,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 0, 45, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { 0, -45, 0 }
 
 // Enable and set to use a specific tool for probing. Disable to allow any tool.
 #define PROBING_TOOL 0
