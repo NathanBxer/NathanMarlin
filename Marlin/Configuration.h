c703a8a12c48665b279a8ba1ca5f8893c77cd839
NathanBxer <84220506+NathanBxer@users.noreply.github.com>
2024-06-22 12:20:49 +1000

    homing feedrate fix again
    as above

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2303,7 +2303,7 @@
 #endif
 
 // Homing speeds (linear=mm/min, rotational=°/min)
-#define HOMING_FEEDRATE_MM_M { (80*60), (80*60), (10*60) }
+#define HOMING_FEEDRATE_MM_M { (50*60), (50*60), (10*60) }
 
 // Validate that endstops are triggered on homing moves
 #define VALIDATE_HOMING_ENDSTOPS
